d1a4d9d496a39c449fea1dcfa55c34c69aad54bc
PaulMercie <paulmercie453@gmail.com>
2019-01-14 12:40:57 +0100

    :sparkles: Creation of a cube in WebGL

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ scene.add(camera)
  * Mesh
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0x87CEFA, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -34,7 +34,40 @@ scene.add(mesh)
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(sizes.width, sizes.height)
 document.body.appendChild(renderer.domElement)
-renderer.render(scene, camera)
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    // Update Mesh
+    mesh.rotation.y += 0.005
+
+    // Update Camera
+    camera.position.x = cursor.x * 3
+    camera.position.y = - cursor.y * 3
+    camera.lookAt(new THREE.Vector3())
+
+    // Render
+    renderer.render(scene, camera)
+}
+
+loop()
 
 // // Hot reload
 // if(module.hot)
