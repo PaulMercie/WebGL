115eebba6c318a026c47902278ac8930933af3d3
PaulMercie <paulmercie453@gmail.com>
2019-01-14 12:03:44 +0100

    :tada: Initialisation of WebGL and creation of a form

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import './css/style.styl'
+import * as THREE from 'three'
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Mesh
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+document.body.appendChild(renderer.domElement)
+renderer.render(scene, camera)
+
+// // Hot reload
+// if(module.hot)
+// {
+//     module.hot.accept()
+
+//     module.hot.dispose(() =>
+//     {
+//         document.body.removeChild($image)
+//     })
+// }
